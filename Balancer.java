76f27bd6e6e3ea929b7784bcf9380a3dc01f0d80
Geno <30701970+IcySkull@users.noreply.github.com>
2023-05-05 12:45:45 -0500

    first

---
+++ Balancer.java
@@ -0,0 +1,15 @@
+public class Balancer{
+    //Map<String type, Set<Element>>() reactionTypes = new HashMap();
+    Map<Element elm, Integer mols>;
+    Element reactantOne;
+    Element reactantTwo;
+    Element reactantThree;
+    Element productOne;
+    Element productTwo;
+    Element productThree;
+    public Balancer(){
+    }
+
+    private combustionReaction
+
+}
--- Element.java
+++ Element.java
@@ -1,4 +1,4 @@
-public class Element{
+public class Element implements Comparable<Element>{
   int electrons;
   int protons;
   int atomicMass;
@@ -9,10 +9,34 @@ public class Element{
     this.atomicMass = mass;
     this.charge = protons-electrons;
   }
-   public Element(int charge, int protons, int mass){
+  public Element(int charge, int protons, int mass){
     this.electrons = protons-charge;
     this.protons = protons;
     this.atomicMass = mass;
     this.charge = charge;
   }
+  public Element(int charge, int protons){
+    this.charge = protons;
+    this.protons = protons;
+    this.electrons = protons - charge;
+  }
+  public Element(int charge){
+    this.charge = charge;
+  }
+  
+  public int getCharge(){
+    return this.charge;
+  }
+  public int getChargeType(){
+    if(this.charge == 0)return 0;
+    if(this.charge >= 1)return 1;
+    if(this.charge <= -1)return -1;
+    return null;
+  }
+
+  @Override
+  public int compareTo(Element o){
+    return getCharge().compareTo(o)
+  }
+
 }
